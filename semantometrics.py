2e81d43bdf89391df8d27b4a6f48751fd59a91ed
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-01-14 10:13:57 +0100

    Refactoring semantometrics script

--- semantometrics.py
+++ semantometrics.py
@@ -6,56 +6,74 @@ import numpy as np
 import nltk
 import string
 
+class Similarity: 
+    """Calculates the similarity of a set of files"""
+    files = set()
+
+    def __init__(self, files):
+        self.files = files
+
+    def tokenize(self, text):
+        """Tokenizes, removes punctuation and stems the given text.""" 
+        words = nltk.word_tokenize(text)
+        tokens = filter(lambda w: w not in string.punctuation, words)
+        stemmer = nltk.stem.snowball.EnglishStemmer()
+        #stemmer = nltk.stem.porter.PorterStemmer() # Alternative stemmer
+        return [stemmer.stem(t) for t in tokens]
+
+    def pairwise_similarity(self): 
+        """
+        Returns the cosine similarity of a set of files.
+        Copied (mostly) from http://stackoverflow.com/a/8897648/3710392
+        """
+        documents = list()
+        for f in self.files: 
+            with open(f, 'r') as doc: 
+                r = doc.read()
+                documents.append(r)
+                #print tokenize(r.decode('utf-8'))
+
+        tfidf = TfidfVectorizer(tokenizer=self.tokenize, stop_words='english').fit_transform(documents)
+        return (tfidf * tfidf.T).A
+        # retrieving a specific value: print ((tfidf * tfidf.T).A)[0,1]
+
+    def pairwise_distance(self):
+        """Returns the pairwise distance (cosine similarity normalized)."""
+        l = len(self.files)
+        if l == 1: 
+            return 1
+        else: 
+            ps = self.pairwise_similarity()
+            return (1 / (l * (l - 1))) * distance_sum(ps)
+
+def distance_sum(matrix):
+    """Returns the total sum of 1 - matrix, minus the main diagonal.
+
+    >>> print distance_sum(np.matrix('1 .2 .3; .4 1 .5; .6 .7 1'))
+    3.3
+    """
+    matrix = 1 - matrix
+    np.fill_diagonal(matrix, 0)
+    return matrix.sum()
+
 def main(): 
     """Retrieves the files and calculates the Contribution score."""
     a_files = glob.glob('a*.txt')
     b_files = glob.glob('b*.txt')
 
-    a = pairwise_distance(b_files) / pairwise_distance(a_files)
+    # Create Similarity objects
+    s_a = Similarity(a_files)
+    s_b = Similarity(b_files)
+    s_ab = Similarity(a_files + b_files)
+
+    # Calculate the parts of the Contribution score
+    a = s_b.pairwise_distance() / s_a.pairwise_distance()
     b = 1 / (len(a_files) * len(b_files))
-    c = distance_sum(pairwise_similarity(a_files + b_files))
+    c = distance_sum(s_ab.pairwise_similarity())
 
-    #print a, b, c, a * b * c
     print a * b * c
-
-def tokenize(text):
-    """Tokenizes, removes punctuation and stems the given text.""" 
-    words = nltk.word_tokenize(text)
-    tokens = filter(lambda w: w not in string.punctuation, words)
-    stemmer = nltk.stem.snowball.EnglishStemmer()
-    #stemmer = nltk.stem.porter.PorterStemmer() # Alternative stemmer
-    return [stemmer.stem(t) for t in tokens]
-
-def pairwise_similarity(files): 
-    """
-    Returns the cosine similarity of a set of files.
-    Copied (mostly) from http://stackoverflow.com/a/8897648/3710392
-    """
-    documents = list()
-    for f in files: 
-        with open(f, 'r') as doc: 
-            r = doc.read()
-            documents.append(r)
-            #print tokenize(r.decode('utf-8'))
-
-    tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english').fit_transform(documents)
-    return (tfidf * tfidf.T).A
-    # retrieving a specific value: print ((tfidf * tfidf.T).A)[0,1]
-
-def distance_sum(matrix):
-    """Returns the total sum of 1 - matrix, minus the main diagonal."""
-    matrix = 1 - matrix
-    np.fill_diagonal(matrix, 0)
-    return matrix.sum()
-
-def pairwise_distance(files):
-    """Returns the pairwise distance (cosine similarity normalized)."""
-    l = len(files)
-    if l == 1: 
-        return 1
-    else: 
-        ps = pairwise_similarity(files)
-        return (1 / (l * (l - 1))) * distance_sum(ps)
 
 if __name__ == "__main__":
     main()
+    #import doctest
+    #doctest.testmod()
